8df078b4e59b7b034b760b9e8cee6bc347b80d89
Elias Ylänen <elias.ylanen@gmail.com>
2019-05-02 14:00:17 +0300

    Do not test build directory; Add utility types

--- types.ts
+++ types.ts
@@ -1,7 +1,16 @@
-interface AnyObj {
+export interface AnyObj {
   [key: string]: any;
 }
 
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
-export { AnyObj, Omit };
+export type Head<T extends any[]> = T[0];
+
+export type Tail<T extends any[]> = ((...args: T) => any) extends ((
+  _: any,
+  ...tail: infer TT
+) => any)
+  ? TT
+  : [];
+
+export type HasTail<T extends any[]> = T extends ([] | [any]) ? false : true;
